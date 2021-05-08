160c1b9057a5e323478af671ec28af4633f4c913
Duc Anh Trinh <trinhducanh1110@gmail.com>
2021-05-08 16:39:49 +0700

    Initial commit

---
+++ WebContent/WEB-INF/views/Web/home.jsp
@@ -0,0 +1,11 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <body>
+        <h1>MacbookStore Spring MVC 1 2 3</h1>
+
+    </body>
+</html>
---
+++ src/main/java/com/MacbookStore/model/Customer.java
@@ -0,0 +1,30 @@
+package com.MacbookStore.model;
+
+public class Customer {
+    public String name;
+    public int age;
+
+    public Customer() {
+    }
+
+    public Customer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
