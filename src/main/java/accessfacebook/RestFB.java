23bf087d41ec1a38d72575ad2c46e7fcd7644e45
Duc Anh Trinh <trinhducanh1110@gmail.com>
2021-05-28 17:13:09 +0700

    Create RestFB.java

---
+++ src/main/java/accessfacebook/RestFB.java
@@ -0,0 +1,50 @@
+package accessfacebook;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Request;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Version;
+
+@Component
+public class RestFB {
+
+    public static String FACEBOOK_APP_ID = "159227136102644";
+    public static String FACEBOOK_APP_SECRET = "77c77be9bcfdeb5217a69e29441e23d0";
+    public static String FACEBOOK_REDIRECT_URL = "https://localhost:8080/AccessFacebook/login-facebook";
+    public static String FACEBOOK_LINK_GET_TOKEN = "https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&redirect_uri=%s&code=%s";
+
+    public String getToken(final String code) throws ClientProtocolException, IOException {
+        String link = String.format(FACEBOOK_LINK_GET_TOKEN, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, FACEBOOK_REDIRECT_URL, code);
+        String response = Request.Get(link).execute().returnContent().asString();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.readTree(response).get("access_token");
+        return node.textValue();
+    }
+
+    public com.restfb.types.User getUserInfo(final String accessToken) {
+        FacebookClient facebookClient = new DefaultFacebookClient(accessToken, FACEBOOK_APP_SECRET, Version.LATEST);
+        return facebookClient.fetchObject("me", com.restfb.types.User.class);
+    }
+    public UserDetails buildUser(com.restfb.types.User user) {
+        boolean enabled = true;
+        boolean accountNonExpired = true;
+        boolean credentialsNonExpired = true;
+        boolean accountNonLocked = true;
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        UserDetails userDetail = new User(user.getId(), "", enabled, accountNonExpired, credentialsNonExpired,
+                accountNonLocked, authorities);
+        return userDetail;
+    }
+}
