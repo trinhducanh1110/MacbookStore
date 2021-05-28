f675847a99092ed2cc354ed692061107bc118047
Duc Anh Trinh <trinhducanh1110@gmail.com>
2021-05-28 10:39:25 +0700

    Update ProductViewModel.java

--- src/main/java/com/MacbookStore/ViewModel/ProductViewModel.java
+++ src/main/java/com/MacbookStore/ViewModel/ProductViewModel.java
@@ -1,4 +1,23 @@
 package com.MacbookStore.ViewModel;
 
 public class ProductViewModel {
+    final String Color;
+    final String Cpu;
+    final String DisplayCard;
+    final String Display;
+    final String Group;
+    final String HardDrive;
+    final String Ram;
+    final String Year;
+
+    public ProductViewModel(String color, String cpu, String displayCard, String display, String group, String hardDrive, String ram, String year) {
+        Color = color;
+        Cpu = cpu;
+        DisplayCard = displayCard;
+        Display = display;
+        Group = group;
+        HardDrive = hardDrive;
+        Ram = ram;
+        Year = year;
+    }
 }
